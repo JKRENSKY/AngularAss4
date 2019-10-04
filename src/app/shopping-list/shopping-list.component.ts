f4a0bd0b5d2fa61a95a7ef0ca6d3eb9b1bbfc088
Joe K <36493556+JKRENSKY@users.noreply.github.com>
2019-10-04 11:55:40 -0700

    taco

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+  new Ingredient('ASADA', 3),
+  new Ingredient('POLLO', 3),
+  new Ingredient('AL PASTOR', 3),
+  new Ingredient('QUESO', 2),
+  new Ingredient('PICO DE GALLO', 2),
+  new Ingredient('CILANTRO', 1),
+  new Ingredient('CREMA', 1),
+  new Ingredient('GUACAMOLE', 2),
+  ];
   constructor() { }
 
   ngOnInit() {
